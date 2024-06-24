cd46b4616d57f4cd2d359e4d451b27e208a488b9
yibei333 <Developer333@hotmail.com>
2024-06-24 17:02:53 +0800

    HASH

--- src/SharpDevLib.Tests/Standard/Extensions/HashExtensionTests.cs
+++ src/SharpDevLib.Tests/Standard/Extensions/HashExtensionTests.cs
@@ -9,7 +9,7 @@ public class HashExtensionTests
 {
     static readonly byte[] _emptyBytes = [];
     static readonly byte[] _bytes = "foobar".ToUtf8Bytes();
-    const string _secret = "123456";
+    static readonly byte[] _secret = "123456".ToUtf8Bytes();
 
     #region MD5
     [TestMethod]
@@ -25,29 +25,7 @@ public class HashExtensionTests
         var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
         var stream = File.OpenRead(path);
         Assert.AreEqual("98f97a791ef1457579a5b7e88a495063", stream.MD5Hash());
-        Assert.AreEqual("1ef1457579a5b7e8", stream.MD5Hash(MD5Length.Sixteen));
-    }
-
-    [TestMethod]
-    public void HMACMD5HashBytesTest()
-    {
-        Assert.AreEqual("cab1380ea86d8acc9aa62390a58406aa", _emptyBytes.MD5Hash(_secret));
-        Assert.AreEqual("16e0cbbcfe1b7e7716e50b1787442bca", _bytes.MD5Hash(_secret));
-    }
-
-    [TestMethod]
-    public void HMACMD5HashStreamTest()
-    {
-        var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
-        var stream = File.OpenRead(path);
-        Assert.AreEqual("2a5578d2b2dd93e0978cad09dd190f8f", stream.MD5Hash(_secret));
-    }
-
-    [TestMethod]
-    [ExpectedException(typeof(InvalidOperationException))]
-    public void HMACMD5HashExceptionTest()
-    {
-        _bytes.MD5Hash("01234567890123456789012345678901234567890123456789012345678901234");
+        Assert.AreEqual("1ef1457579a5b7e8", stream.MD5Hash(MD5OutputLength.Sixteen));
     }
     #endregion
 
@@ -67,22 +45,6 @@ public class HashExtensionTests
         var stream = File.OpenRead(path);
         Assert.AreEqual("16ad856b462e68f965f6e93f66282a7ae891fdbc", stream.SHA128Hash());
     }
-
-    [TestMethod]
-    public void HMACSHA128HashBytesTest()
-    {
-        Assert.AreEqual("823688dafca7393d24c871a2da98a84d8732e927", _emptyBytes.SHA128Hash(_secret));
-        Assert.AreEqual("434d11195a10d3df19b0fcebc6c0c147e3bc5ffa", _bytes.SHA128Hash(_secret));
-    }
-
-    [TestMethod]
-    public void HMACSHA128HashStreamTest()
-    {
-        Assert.AreEqual("823688dafca7393d24c871a2da98a84d8732e927", new MemoryStream().SHA128Hash(_secret));
-        var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
-        var stream = File.OpenRead(path);
-        Assert.AreEqual("8dbc2c077885f13bbb1e15171a4971a4d87cff50", stream.SHA128Hash(_secret));
-    }
     #endregion
 
     #region SHA256
@@ -100,22 +62,6 @@ public class HashExtensionTests
         var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
         var stream = File.OpenRead(path);
         Assert.AreEqual("8f4ec1811c6c4261c97a7423b3a56d69f0f160074f39745af20bb5fcf65ccf78", stream.SHA256Hash());
-    }
-
-    [TestMethod]
-    public void HMACSHA256HashBytesTest()
-    {
-        Assert.AreEqual("b946ccc987465afcda7e45b1715219711a13518d1f1663b8c53b848cb0143441", _emptyBytes.SHA256Hash(_secret));
-        Assert.AreEqual("9708280ad4dad450336407679f91be06a815ec7aacf8e489e0c2e5dcbd103087", _bytes.SHA256Hash(_secret));
-    }
-
-    [TestMethod]
-    public void HMACSHA256HashStreamTest()
-    {
-        Assert.AreEqual("b946ccc987465afcda7e45b1715219711a13518d1f1663b8c53b848cb0143441", new MemoryStream().SHA256Hash(_secret));
-        var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
-        var stream = File.OpenRead(path);
-        Assert.AreEqual("0b813213ecb33353e90ccf0f94ccbbf5f351ba7b9bef10f848e9e7e215729f7e", stream.SHA256Hash(_secret));
     }
     #endregion
 
@@ -135,22 +81,6 @@ public class HashExtensionTests
         var stream = File.OpenRead(path);
         Assert.AreEqual("75cc227fe8076e0456123113694e0fed43d28f45f8a4a67894732fe9893b0ab6194cb86b57f5f67316263382e2d72c4b", stream.SHA384Hash());
     }
-
-    [TestMethod]
-    public void HMACSHA384HashBytesTest()
-    {
-        Assert.AreEqual("e803f868ad733ea86c6d68c7f8ed6727f5e48d8bf40bcd7f07afe69324fec6567d547c1b7eeabfa33ef3bc34bf9011c8", _emptyBytes.SHA384Hash(_secret));
-        Assert.AreEqual("7773f91bd2e7da73f676ab9da486c517d1efb0065ac49eaf1d28f70e4b1a3c5f6705a04de6e614d8e5099e8e30c8d7f3", _bytes.SHA384Hash(_secret));
-    }
-
-    [TestMethod]
-    public void HMACSHA384HashStreamTest()
-    {
-        Assert.AreEqual("e803f868ad733ea86c6d68c7f8ed6727f5e48d8bf40bcd7f07afe69324fec6567d547c1b7eeabfa33ef3bc34bf9011c8", new MemoryStream().SHA384Hash(_secret));
-        var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
-        var stream = File.OpenRead(path);
-        Assert.AreEqual("90e32a38d270e5c2c43845768e1d449e7a85803b24d822792e1a994dfd2d6e19577bde568d8c98e933d5f7d5953e6ccd", stream.SHA384Hash(_secret));
-    }
     #endregion
 
     #region SHA512
@@ -169,21 +99,101 @@ public class HashExtensionTests
         var stream = File.OpenRead(path);
         Assert.AreEqual("75fbed00cbfdfaa6eba25791df285ec7fd6135fe8443adf86d4dc2bbfe061e84c3061e280c01366286c0366276976903666627c6d8e0a69b192c665bae8adb0b", stream.SHA512Hash());
     }
+    #endregion
 
+    #region HMACMD5
+    [TestMethod]
+    public void HMACMD5HashBytesTest()
+    {
+        Assert.AreEqual("cab1380ea86d8acc9aa62390a58406aa", _emptyBytes.HMACMD5Hash(_secret));
+        Assert.AreEqual("16e0cbbcfe1b7e7716e50b1787442bca", _bytes.HMACMD5Hash(_secret));
+    }
+
+    [TestMethod]
+    public void HMACMD5HashStreamTest()
+    {
+        var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
+        var stream = File.OpenRead(path);
+        Assert.AreEqual("2a5578d2b2dd93e0978cad09dd190f8f", stream.HMACMD5Hash(_secret));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void HMACMD5HashExceptionTest()
+    {
+        _bytes.HMACMD5Hash("01234567890123456789012345678901234567890123456789012345678901234".ToUtf8Bytes());
+    }
+    #endregion
+
+    #region HMACSHA128
+    [TestMethod]
+    public void HMACSHA128HashBytesTest()
+    {
+        Assert.AreEqual("823688dafca7393d24c871a2da98a84d8732e927", _emptyBytes.HMACSHA128Hash(_secret));
+        Assert.AreEqual("434d11195a10d3df19b0fcebc6c0c147e3bc5ffa", _bytes.HMACSHA128Hash(_secret));
+    }
+
+    [TestMethod]
+    public void HMACSHA128HashStreamTest()
+    {
+        Assert.AreEqual("823688dafca7393d24c871a2da98a84d8732e927", new MemoryStream().HMACSHA128Hash(_secret));
+        var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
+        var stream = File.OpenRead(path);
+        Assert.AreEqual("8dbc2c077885f13bbb1e15171a4971a4d87cff50", stream.HMACSHA128Hash(_secret));
+    }
+    #endregion
+
+    #region HMACSHA256
+    [TestMethod]
+    public void HMACSHA256HashBytesTest()
+    {
+        Assert.AreEqual("b946ccc987465afcda7e45b1715219711a13518d1f1663b8c53b848cb0143441", _emptyBytes.HMACSHA256Hash(_secret));
+        Assert.AreEqual("9708280ad4dad450336407679f91be06a815ec7aacf8e489e0c2e5dcbd103087", _bytes.HMACSHA256Hash(_secret));
+    }
+
+    [TestMethod]
+    public void HMACSHA256HashStreamTest()
+    {
+        Assert.AreEqual("b946ccc987465afcda7e45b1715219711a13518d1f1663b8c53b848cb0143441", new MemoryStream().HMACSHA256Hash(_secret));
+        var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
+        var stream = File.OpenRead(path);
+        Assert.AreEqual("0b813213ecb33353e90ccf0f94ccbbf5f351ba7b9bef10f848e9e7e215729f7e", stream.HMACSHA256Hash(_secret));
+    }
+    #endregion
+
+    #region HMACSHA384
+    [TestMethod]
+    public void HMACSHA384HashBytesTest()
+    {
+        Assert.AreEqual("e803f868ad733ea86c6d68c7f8ed6727f5e48d8bf40bcd7f07afe69324fec6567d547c1b7eeabfa33ef3bc34bf9011c8", _emptyBytes.HMACSHA384Hash(_secret));
+        Assert.AreEqual("7773f91bd2e7da73f676ab9da486c517d1efb0065ac49eaf1d28f70e4b1a3c5f6705a04de6e614d8e5099e8e30c8d7f3", _bytes.HMACSHA384Hash(_secret));
+    }
+
+    [TestMethod]
+    public void HMACSHA384HashStreamTest()
+    {
+        Assert.AreEqual("e803f868ad733ea86c6d68c7f8ed6727f5e48d8bf40bcd7f07afe69324fec6567d547c1b7eeabfa33ef3bc34bf9011c8", new MemoryStream().HMACSHA384Hash(_secret));
+        var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
+        var stream = File.OpenRead(path);
+        Assert.AreEqual("90e32a38d270e5c2c43845768e1d449e7a85803b24d822792e1a994dfd2d6e19577bde568d8c98e933d5f7d5953e6ccd", stream.HMACSHA384Hash(_secret));
+    }
+    #endregion
+
+    #region HMACSHA512
     [TestMethod]
     public void HMACSHA512HashBytesTest()
     {
-        Assert.AreEqual("d3f2f066f0da13b4cd51085457a9c50f4dfc3ddc2b790133d49f6a11bd048ab7bf4292abaae52d5c2841f7eda24f51bce0858ef75dd0ee02283c73783d63c6a4", _emptyBytes.SHA512Hash(_secret));
-        Assert.AreEqual("60287c042116ec7cc71ccc442bd961117a1a3a059b4debe5cc9e87856a2ffabf1f47c2fddf24e3a0b4eb0fb07e15d980dac9cf4f7bdd1548ef5db8d9acb79317", _bytes.SHA512Hash(_secret));
+        Assert.AreEqual("d3f2f066f0da13b4cd51085457a9c50f4dfc3ddc2b790133d49f6a11bd048ab7bf4292abaae52d5c2841f7eda24f51bce0858ef75dd0ee02283c73783d63c6a4", _emptyBytes.HMACSHA512Hash(_secret));
+        Assert.AreEqual("60287c042116ec7cc71ccc442bd961117a1a3a059b4debe5cc9e87856a2ffabf1f47c2fddf24e3a0b4eb0fb07e15d980dac9cf4f7bdd1548ef5db8d9acb79317", _bytes.HMACSHA512Hash(_secret));
     }
 
     [TestMethod]
     public void HMACSHA512HashStreamTest()
     {
-        Assert.AreEqual("d3f2f066f0da13b4cd51085457a9c50f4dfc3ddc2b790133d49f6a11bd048ab7bf4292abaae52d5c2841f7eda24f51bce0858ef75dd0ee02283c73783d63c6a4", new MemoryStream().SHA512Hash(_secret));
+        Assert.AreEqual("d3f2f066f0da13b4cd51085457a9c50f4dfc3ddc2b790133d49f6a11bd048ab7bf4292abaae52d5c2841f7eda24f51bce0858ef75dd0ee02283c73783d63c6a4", new MemoryStream().HMACSHA512Hash(_secret));
         var path = AppDomain.CurrentDomain.BaseDirectory.CombinePath("TestData/TestFile.txt");
         var stream = File.OpenRead(path);
-        Assert.AreEqual("6ad68a997b23d75c73f13872b8f56f4841f27b4d91cc4cbf94211a57d61b98e5442abd1f3363e25eead1efc0e70e7f438f118b2fe0050d728ecd07958d2a3d99", stream.SHA512Hash(_secret));
+        Assert.AreEqual("6ad68a997b23d75c73f13872b8f56f4841f27b4d91cc4cbf94211a57d61b98e5442abd1f3363e25eead1efc0e70e7f438f118b2fe0050d728ecd07958d2a3d99", stream.HMACSHA512Hash(_secret));
     }
     #endregion
 }
