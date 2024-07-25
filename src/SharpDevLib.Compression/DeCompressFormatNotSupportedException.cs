e727485eff755bab1ae24d8da43fde50c5d9cc05
yibei333 <Developer333@hotmail.com>
2024-07-25 18:31:16 +0800

    class

--- src/SharpDevLib.Compression/DeCompressFormatNotSupportedException.cs
+++ src/SharpDevLib.Compression/DeCompressFormatNotSupportedException.cs
@@ -3,31 +3,6 @@
 namespace SharpDevLib.Compression;
 
 /// <summary>
-/// 压缩不支持的格式异常
-/// </summary>
-public class CompressFormatNotSupportedException : NotSupportedException
-{
-    /// <summary>
-    /// 实例化异常
-    /// </summary>
-    /// <param name="extension">文件扩展名</param>
-    public CompressFormatNotSupportedException(string extension) : base($"extension '{extension}' not supported,supported format is '{string.Join(",", InternalCompressionExtension.SupportedCompressExtensions)}'")
-    {
-        Extension = extension;
-    }
-
-    /// <summary>
-    /// 支持的格式
-    /// </summary>
-    public List<string> SupportedFormats => InternalCompressionExtension.SupportedCompressExtensions;
-
-    /// <summary>
-    /// 文件扩展名
-    /// </summary>
-    public string Extension { get; }
-}
-
-/// <summary>
 /// 解压不支持的格式异常
 /// </summary>
 public class DeCompressFormatNotSupportedException : NotSupportedException
