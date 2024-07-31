a7421b77067a39ef549f7800e905026639b8027a
yibei333 <Developer333@hotmail.com>
2024-07-31 16:11:47 +0800

    reflection

--- src/SharpDevLib/Extensions/ReflectionExtension.cs
+++ src/SharpDevLib/Extensions/ReflectionExtension.cs
@@ -16,15 +16,26 @@ public static class ReflectionExtension
     /// <returns>名称</returns>
     public static string GetTypeDefinitionName(this Type type, bool isFullName = false)
     {
-        if (!type.IsGenericType) return isFullName ? type.FullName ?? type.Name : type.Name;
+        if (!type.IsGenericType) return type.GetTypeName(isFullName);
 
         var names = new List<string>();
         foreach (var item in type.GetGenericArguments())
         {
             names.Add(GetTypeDefinitionName(item, isFullName));
         };
-        var typeName = isFullName ? type.FullName ?? type.Name : type.Name;
+        var typeName = type.GetTypeName(isFullName);
         return $"{typeName.Split('`')[0]}<{string.Join(",", names)}>";
+    }
+
+    static string GetTypeName(this Type type, bool isFullName)
+    {
+        if (!isFullName) return type.Name;
+        var typeName = type.FullName;
+        if (typeName.IsNullOrWhiteSpace())
+        {
+            typeName = $"{type.Namespace}.{type.Name}";
+        }
+        return typeName;
     }
 
     /// <summary>
@@ -39,8 +50,9 @@ public static class ReflectionExtension
     /// 获取方法定义名称
     /// </summary>
     /// <param name="methodInfo">methodInfo</param>
+    /// <param name="isFullName">是否全名</param>
     /// <returns>方法定义名称</returns>
-    public static string GetMethodDefinitionName(this MethodInfo methodInfo)
+    public static string GetMethodDefinitionName(this MethodInfo methodInfo, bool isFullName = false)
     {
         var builder = new StringBuilder();
         builder.Append(methodInfo.Name);
@@ -54,10 +66,39 @@ public static class ReflectionExtension
         var parameters = methodInfo.GetParameters();
         if (parameters.Any())
         {
-            builder.Append(string.Join(",", methodInfo.GetParameters().Select(x => $"{x.ParameterType.GetTypeDefinitionName()} {x.Name}")));
+            builder.Append(string.Join(",", methodInfo.GetParameters().Select(x => $"{x.ParameterType.GetTypeDefinitionName(isFullName)} {x.Name}")));
+        }
+        builder.Append(')');
+
+        return builder.ToString();
+    }
+
+    /// <summary>
+    /// 获取构造方法定义名称
+    /// </summary>
+    /// <param name="constructorInfo">constructorInfo</param>
+    /// <param name="containParameterName">是否包含参数名称</param>
+    /// <param name="isFullName">是否全名</param>
+    /// <returns>构造方法定义名称</returns>
+    public static string GetConstructorDefinitionName(this ConstructorInfo constructorInfo, bool containParameterName, bool isFullName = false)
+    {
+        var builder = new StringBuilder();
+        builder.Append(constructorInfo.DeclaringType?.Name?.Split('`')[0]);
+        if (constructorInfo.IsGenericMethod)
+        {
+            builder.Append('<');
+            builder.Append(string.Join(",", constructorInfo.GetGenericArguments().Select(x => x.Name)));
+            builder.Append('>');
+        }
+        builder.Append('(');
+        var parameters = constructorInfo.GetParameters();
+        if (parameters.Any())
+        {
+            builder.Append(string.Join(",", parameters.Select(x => $"{x.ParameterType.GetTypeDefinitionName(isFullName)}{(containParameterName ? $" {x.Name}" : "")}")));
         }
         builder.Append(')');
 
         return builder.ToString();
     }
 }
+
